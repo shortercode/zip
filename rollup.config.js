d70ea07fcea8512c68d36e399a88b73c79863b7a
shortercode <iainshorter@gmail.com>
2019-12-21 16:11:48 +0000

    Update rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,16 @@
-module.exports = {
-    input: 'temp/ZipArchive.js',
+module.exports = [
+  {
+    input: 'temp/deflate_helper.js',
     output: {
-      file: 'dist/zip.js',
+      file: 'dist/zip_default.js',
       format: 'es'
     }
-  };
+  },
+  {
+    input: 'temp/ZipArchive.js',
+    output: {
+      file: 'dist/zip_store_only.js',
+      format: 'es'
+    }
+  }
+];
