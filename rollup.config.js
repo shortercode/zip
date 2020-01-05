f4010a35cfcca0d5fab51e59f31582693950ce13
shortercode <iainshorter@gmail.com>
2020-01-05 14:38:24 +0000

    Update rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -12,5 +12,21 @@ module.exports = [
       file: 'dist/zip_store_only.js',
       format: 'es'
     }
+	},
+	{
+    input: 'temp/deflate_helper.js',
+    output: {
+			file: 'dist/zip_default.umd.js',
+			name: 'zip',
+      format: 'umd'
+    }
+  },
+  {
+    input: 'temp/ZipArchive.js',
+    output: {
+			file: 'dist/zip_store_only.umd.js',
+			name: 'zip',
+      format: 'umd'
+    }
   }
 ];
