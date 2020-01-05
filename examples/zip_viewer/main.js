a62eb85f898a70a66f4de1696a813ee9cf4affe9
shortercode <iainshorter@gmail.com>
2020-01-05 19:23:58 +0000

    More work on the file drop zone

--- examples/zip_viewer/main.js
+++ examples/zip_viewer/main.js
@@ -35,53 +35,73 @@ class RootComponent extends Component {
     }
 }
 
-function DropZoneExplainerComponent () {
-    return createElement(Fragment, {}, "Drop a zip file here to begin");
-}
-
-function DropZoneComponent ({ on_archive_change }) {
-    function read_file (file) {
-        // TODO show busy when reading blob
-        ZipArchive.from_blob(file).then(
-            archive => {
-                on_archive_change(archive);
-            },
-            error => {
-                // TODO show error when invalid file
-            }
-        )
+class DropZoneComponent extends Component {
+    constructor (props) {
+        super(props);
+        this.state = { is_drag_active: false, is_busy: false, error_message: "" };
     }
+    on_drop (e) {
+        e.preventDefault();
+        const files = files_from_data_transfer(e.dataTransfer);
 
-    return createElement(
-        "div",
-        { 
-            className: "",
-            onDrop: e => {
-                e.preventDefault();
-                const files = files_from_data_transfer(e.dataTransfer);
+        if (files.length > 1) {
+            this.setState({ error_message: "Please drop only 1 file" });
+            return;
+        }
 
-                if (files.length > 1) {
-                    // TODO show warning when more than 1 file is dropped
-                    console.warn("too many files");
-                    return;
-                }
+        this.read_file(files[0]);
+    }
+    on_drag_over (e) {
+        e.preventDefault();
+        this.setState({ is_drag_active: true });
+    }
+    on_drag_leave (e) {
+        this.setState({ is_drag_active: false });
+    }
+    async read_file (file) {
+        this.setState({ is_busy: true, error_message: "", is_drag_active: false });
+        try {
+            const archive = await ZipArchive.from_blob(file);
+            this.props.on_archive_change(archive);
+        }
+        catch (e) {
+            this.setState({ error_message: "Unable to read the file, is it a Zip file?" });
+        }
+        finally {
+            this.setState({ is_busy: false });
+        }
+    }
+    render () {
+        let text = "Drop a zip file here or click and select a file to begin";
 
-                read_file(files[0]);
+        if (this.state.is_busy) {
+            text = "Processing file...";
+        }
+        else if (this.state.is_drag_active) {
+            text = "Drop file here...";
+        }
+        else if (this.state.error_message) {
+            text = this.state.error_message;
+        }
+
+        return createElement(
+            "label",
+            { 
+                className: "drop-zone",
+                onDrop: e => this.on_drop(e),
+                onDragover: e => this.on_drag_over(e),
+                onDragleave: e => this.on_drag_leave(e) 
             },
-            onDrag: e => {
-                e.preventDefault();
-                // TODO show preview
-            }
-        },
-        createElement(DropZoneExplainerComponent),
-        createElement(
-            "input",
-            {
-                type: "file",
-                onChange: e => read_file(e.files[0])
-            }
-        )
-    );
+            createElement(Fragment, {}, text),
+            createElement(
+                "input",
+                {
+                    type: "file",
+                    onChange: e => this.read_file(e.files[0])
+                }
+            )
+        );
+    }
 }
 
 function ViewerComponent (props) {
