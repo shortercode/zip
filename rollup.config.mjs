1e08b7deeda97afef897c76f6fbf31bb2fbdf680
Iain Shorter <iainshorter@gmail.com>
2023-04-18 22:15:23 +0100

    use prerelease label instead of build in version

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -8,7 +8,7 @@ function build_revision_plugin () {
   return {
     name: 'build-revision',
     buildEnd () {
-      const version = `${pkg.version}+${process.env.BUILD_NUMBER || 0}`;
+      const version = `${pkg.version}-${process.env.BUILD_NUMBER || 0}`;
       console.log({ version });
       return fs.promises.writeFile('./dist/package.json', JSON.stringify({
         ...pkg,
